74948608d03e1f45a28e4093c33f9e746e50efc5
Hamdy <you@example.com>
2023-02-22 18:09:53 -0800

    print sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Entry point, add two integers and return the answer
+ * @x: first integer
+ * @y: second integer
+ * Return: The answer of the sum of x and y
+ */
+int add(int x, int y)
+{
+	int ans;
+
+	ans = x + y;
+
+	return (ans);
+}
