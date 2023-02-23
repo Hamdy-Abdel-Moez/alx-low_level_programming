16291bc39b73a92050a01b1aa3d11e943bb9b54e
Hamdy <you@example.com>
2023-02-22 16:44:17 -0800

    print alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,13 +5,17 @@
  *
  * Return: void
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
+	int i;
 	char c;
 
-	for (c = 'a'; c <= 'z' ; c++)
+	for (i = 0; i <= 9 ; i++)
 	{
-		_putchar(c);
+		for (c = 'a' ; c <= 'z' ; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
