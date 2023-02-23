e4a6db552dd912af8bfeb154fc1ef6b55dfcbd84
Hamdy <you@example.com>
2023-02-22 17:04:41 -0800

    check lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - Entry point, chech lowercase
+ * @c: the character it receives
+ * Return: 1 if true otherwise 0 if false
+ */
+int _islower(int c)
+{
+	char c, ch1, ch2;
+
+	ch1 = 'a';
+	ch2 = 'z';
+
+	if (c <= ch2 && c >= ch1)
+		return (1);
+	else
+		return (0);
+}
