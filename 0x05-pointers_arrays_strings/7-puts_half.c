2f72ea27d019a8fb69669d91741bdeea4d991fd6
Hamdy <you@example.com>
2023-02-28 15:36:09 -0800

    print half of a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
  * puts_half - prints half of a string
- * followed by a new line
  * @str: string to be printed
  */
 void puts_half(char *str)
@@ -9,7 +8,7 @@ void puts_half(char *str)
 	int len = 0;
 	int i, j;
 
-	while (s[len])
+	while (str[len])
 	{
 		len++;
 	}
